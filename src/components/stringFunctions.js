f28240c6cc612dd9d76e5fc0a489838aa4a48aea
Christian Beckham <urenacb22@gmail.com>
2022-10-27 16:23:19 -0500

    added code for string functions

--- src/components/stringFunctions.js
+++ src/components/stringFunctions.js
@@ -1,5 +1,13 @@
-const capitalizeFirstLetter = () => {};
-const concatTwoStrings = () => {};
-const returnSecondWordInString = () => {};
+const capitalizeFirstLetter = (string) => {
+	return string[0].toUpperCase() + string.substring(1);
+};
+
+const concatTwoStrings = (firstString, secondString) => {
+	return firstString.concat(secondString);
+};
+
+const returnSecondWordInString = (phrase) => {
+	return phrase.split(" ")[1];
+};
 
 export { capitalizeFirstLetter, concatTwoStrings, returnSecondWordInString };
