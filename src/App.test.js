3b3af6d0718ab3d3264653d46d79dddc6bd2cce8
Christian Beckham <urenacb22@gmail.com>
2022-10-27 16:45:56 -0500

    added test to check if logo is rendered

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+	render(<App />);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
+});
+
+it("renders the 'logo.svg' image", () => {
+	render(<App />);
+	const logoElement = screen.getByRole("img");
+	expect(logoElement).toHaveAttribute("src", "logo.svg");
 });
